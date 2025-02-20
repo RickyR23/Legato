ec2229e2ecec64d6c589afa74f2df66734c4ef76
AngelMerchant <angel.merchant.192@my.csun.edu>
2025-02-19 23:50:19 -0800

    WIP Navigation Bar

--- app/src/main/java/com/example/legatoapp/userHome.java
+++ app/src/main/java/com/example/legatoapp/userHome.java
@@ -1,23 +1,31 @@
 package com.example.legatoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.example.legatoapp.databinding.ActivityUserHomeBinding;
 
 public class userHome extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    ActivityUserHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityUserHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Set Home Fragment by Default
+        replaceFragment(new HomeFragment());
+
+        // Logout Button Logic
         Button logoutButton = findViewById(R.id.logout_button);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,8 +33,38 @@ public class userHome extends AppCompatActivity {
                 logoutUser();
             }
         });
+
+        // Bottom Navigation Click Listener (Fixed if-else)
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.home) {
+                replaceFragment(new HomeFragment());
+                return true;
+            } else if (id == R.id.create) {
+                replaceFragment(new CreateFragment());
+                return true;
+            } else if (id == R.id.inbox) {
+                replaceFragment(new InboxFragment());
+                return true;
+            } else if (id == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+                return true;
+            }
+
+            return false;
+        });
     }
 
+    // Replace Fragment Method
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    // Logout User
     private void logoutUser() {
         SharedPreferences sharedPreferences = getSharedPreferences("LegatoPrefs", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
