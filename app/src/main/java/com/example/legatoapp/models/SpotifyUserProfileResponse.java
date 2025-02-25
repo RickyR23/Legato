cf7f85c1166174f55f6dd67e617217ca842267fd
ricky <rickyramos834@gmail.com>
2025-02-25 15:43:48 -0800

    LEGATO-36 - Spotify API: Retrieve user data

---
+++ app/src/main/java/com/example/legatoapp/models/SpotifyUserProfileResponse.java
@@ -0,0 +1,14 @@
+package com.example.legatoapp.models;
+
+import android.media.Image;
+
+import java.util.List;
+import java.util.Map;
+
+public class SpotifyUserProfileResponse {
+    private String display_name;
+    private Map<String, String> external_urls;
+    private List<Image> images;
+
+
+}
