9b7be673a77a308657128b605a8ade36cb0c4a65
Mauricio Andrade <mauricio.andrade.011@my.csun.edu>
2025-03-05 13:23:10 -0800

    added request permission at runtime in BaseActivity

--- app/src/main/java/com/example/legatoapp/BaseActivity.java
+++ app/src/main/java/com/example/legatoapp/BaseActivity.java
@@ -3,4 +3,14 @@ package com.example.legatoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
+    {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 101);
+    }
 }
+
+}
+}
+
+
