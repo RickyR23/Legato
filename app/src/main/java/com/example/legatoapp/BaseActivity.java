67dbf80f1fe6693c24a4649e0b4e127d2068f30b
Mauricio Andrade <mauricio.andrade.011@my.csun.edu>
2025-03-05 13:35:04 -0800

    made changes to BaseActivity

--- app/src/main/java/com/example/legatoapp/BaseActivity.java
+++ app/src/main/java/com/example/legatoapp/BaseActivity.java
@@ -1,16 +1,25 @@
 package com.example.legatoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.os.Bundle;
+
+
 
 public class BaseActivity extends AppCompatActivity {
-    {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-    if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 101);
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 101);
     }
-}
-
-}
-}
-
-
+        }
+    }
+}      
