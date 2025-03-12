0efff4de6df1b0e4f0ab4a35f29b82bc96ada5f6
angelesmontalvo <angeles.montalvo.267@my.csun.edu>
2025-03-12 03:01:28 -0700

    Added editText in EditProfile page to change the Display Name and Bio. Also added a character count that updates as the user types for the bio

--- app/src/main/java/com/example/legatoapp/EditProfileActivity.java
+++ app/src/main/java/com/example/legatoapp/EditProfileActivity.java
@@ -1,6 +1,40 @@
 package com.example.legatoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class EditProfileActivity extends AppCompatActivity {
+    private EditText editBioEditText;
+    private TextView bioCharCount;
+    private static final int MAX_CHAR_COUNT = 80;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        editBioEditText = findViewById(R.id.editBioEditText);
+        bioCharCount = findViewById(R.id.bioCharCount);
+
+        // TextWatcher checks that the bio updates character count
+        editBioEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int currentLength = s.length();
+                bioCharCount.setText(currentLength + "/" + MAX_CHAR_COUNT);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+    }
 }
